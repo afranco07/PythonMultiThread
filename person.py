35333d651f0ccb4ee3ec0dfbb97d17565664bc99
afranco07 <alberto.franco86@qmail.cuny.edu>
2017-07-02 12:07:52 -0400

    Added the person class that will be used to extend the subclasses

--- person.py
+++ person.py
@@ -1,2 +1,9 @@
 """The class where the student and instructor class extend from"""
 
+from classroom import ClassRoom
+
+class Person():
+    """The base class where the instructor and student extend from"""
+
+    class_room = ClassRoom(2, 3, 1)
+    
