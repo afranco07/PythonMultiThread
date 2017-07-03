2e60114a55c18696c8ffac2ae100e3705752f6d2
afranco07 <alberto.franco86@qmail.cuny.edu>
2017-07-03 00:05:42 -0400

    Added the gitignore and started the run method for the students

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from student import Student
 
 s1 = Student()
+s2 = Student()
 s1.start()
+s2.start()
--- student.py
+++ student.py
@@ -2,6 +2,7 @@
 
 import threading
 import time
+import random
 from person import Person
 
 class Student(Person, threading.Thread):
@@ -15,15 +16,19 @@ class Student(Person, threading.Thread):
     leaving_student = 0
 
     def __init__(self):
+        """Constructor"""
         super(Student, self).__init__()
         self.exam_1 = 0
         self.exam_2 = 0
         self.exam_3 = 0
         self.arrival_time = 1000
-        #self.number = id + 1
+        Student.id_number += 1
+        self.number = Student.id_number
 
     def run(self):
         """Implementing the thread run method"""
-        print("The thread has started running...")
-        time.sleep(5)
-        print("All done!")
+        print("The thread " + str(self.number) + " has started running...")
+        sleep_time = random.randrange(5)
+        print(str(self.number) + " is sleeping for "+ str(sleep_time) + " second(s)")
+        time.sleep(sleep_time)
+        print(str(self.number) + " is done!")
